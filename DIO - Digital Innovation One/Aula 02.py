5910ebdb7fbbe8a3672a050062598fd845cfccdf
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-09-23 09:28:41 -0300

    Update Aula 02.py

--- DIO - Digital Innovation One/Aula 02.py
+++ DIO - Digital Innovation One/Aula 02.py
@@ -1,13 +1,22 @@
 a = 10
 b = 5
+
+#Para inserir comentários em Python basta inciar com o símbolo "#" Hashtag.
+
 soma = a + b
 subtracao = a - b
 multiplicacao = a * b
 divisao = a / b
 resto = a % b
+exponenciacao = a ** b
 print(soma)
 print(subtracao)
 print(multiplicacao)
 print(divisao)
 print(resto)
+print(exponenciacao)
+
+#Se desejarmos consultar o 'tipo' de uma variável podemos utilizar a palavra 'type'. veja um exemplo abaixo:
+
+print(type(soma))
 
