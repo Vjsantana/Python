5d2db76373080343758f347cdda2f49357b6f8d6
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-09-23 09:40:26 -0300

    Update Aula 02.py

--- DIO - Digital Innovation One/Aula 02.py
+++ DIO - Digital Innovation One/Aula 02.py
@@ -1,7 +1,10 @@
 a = 10
 b = 5
 
-#Para inserir comentários em Python basta inciar com o símbolo "#" Hashtag.
+#Para inserir comentários em apenas uma linha no Python basta inciar com o símbolo "#" Hashtag.
+
+"""Para inserir comentários em mais de uma linha no Python digite o seu comentário entre três aspas simples ou 
+três aspas duplas. O comentário aparecerá na forma de uma string mas não irá gerar bytecod."""
 
 soma = a + b
 subtracao = a - b
@@ -19,4 +22,6 @@ print(exponenciacao)
 #Se desejarmos consultar o 'tipo' de uma variável podemos utilizar a palavra 'type'. veja um exemplo abaixo:
 
 print(type(soma))
+#O resultado exibido será: <class 'int'>
+
 
