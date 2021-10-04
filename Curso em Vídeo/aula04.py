c8b663d7c3051e54ec38377e35ee2b5eca0da3ee
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-10-04 09:15:46 -0300

    Criação da aula 04 em Python

---
+++ Curso em Vídeo/aula04.py
@@ -0,0 +1,10 @@
+nome = input('Qual é o seu nome?')
+print(nome)
+idade = input('Qual é a sua idade?')
+print(idade)
+endereco = input('Qual é o seu endereço?')
+print(endereco)
+peso = input('Quantos quilos você pesa?')
+print(peso)
+
+print(nome, idade, endereco, peso)
