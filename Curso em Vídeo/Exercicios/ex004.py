724505c9c176d58be6694d83060e81828ccc5845
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-10-08 18:06:08 -0300

    Update ex004.py

--- Curso em Vídeo/Exercicios/ex004.py
+++ Curso em Vídeo/Exercicios/ex004.py
@@ -1,11 +1,13 @@
 algo = input('Digite algo:')
 print('O tipo primitivo desse valor é {}:' .format(type(algo)))
 print('Só tem espaços?', algo.isspace())
-print('É um número?')
-print('É alfabético?')
-print('É alfanumérico?')
-print('Está em letras maiúsculas?')
-print('Está em letras minúsculas?')
-print('Está capitalizada?')
+print('É um número?', algo.isnumeric())
+print('É alfabético?', algo.isalpha())
+print('É alfanumérico?', algo.isalnum())
+print('Está em letras maiúsculas?', algo.isupper())
+#print('Está em letras minúsculas?')
+#print('Está capitalizada?', algo.)
 
-
+"""No caso da opção de apresentar o tipo da variável digitada acima, o 'type' sempre será uma string pois
+a funçao 'input' retorna sempre uma string. Além do mais, não foi especificado, antes da função input, 
+o tipo da variável que desejamos que o programa retorne."""
