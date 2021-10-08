a6afb9858acb6830d68c0195ad4304ab02e25a11
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-10-08 11:48:51 -0300

    Update ex002.py

--- Curso em Vídeo/Exercicios/ex002.py
+++ Curso em Vídeo/Exercicios/ex002.py
@@ -1,2 +1,7 @@
 nome = input('Digite seu nome')
 print('Seja bem vindo' + nome + ' é um prazer tê-lo(a) conosco.')
+
+"""Poderemos também utilizar o '.format' para concatenar strings e variáveis. As chaves '{}' são utilizadas 
+para substituir a variável que desejamos concatenar quando utilizamos o método '.format'. Veja o exemplo a seguir:"""
+
+print('É um prazer te conhecer,{}!' .format(nome))
