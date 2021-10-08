80453bc5a3ca2941e54de94aaa6be76971fc732f
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-10-08 17:44:05 -0300

    Update ex004.py

--- Curso em Vídeo/Exercicios/ex004.py
+++ Curso em Vídeo/Exercicios/ex004.py
@@ -1,3 +1,11 @@
 algo = input('Digite algo:')
 print('O tipo primitivo desse valor é {}:' .format(type(algo)))
-#print('Só tem espeços?', sy)
+print('Só tem espaços?', algo.isspace())
+print('É um número?')
+print('É alfabético?')
+print('É alfanumérico?')
+print('Está em letras maiúsculas?')
+print('Está em letras minúsculas?')
+print('Está capitalizada?')
+
+
