c45bde21e0dbdc6f5cc56c0526c12b301c59e754
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-10-08 15:08:16 -0300

    Update ex003.

--- Curso em Vídeo/Exercicios/ex003.py
+++ Curso em Vídeo/Exercicios/ex003.py
@@ -2,3 +2,8 @@ n1 = int(input('Digite o primeiro número'))
 n2 = int(input('Digite o segundo número'))
 soma = n1 + n2
 print('A soma dos números é:', soma)
+print('A soma entre', n1, 'e', n2, 'vale:', (soma))
+
+# O comando 'print' acima pode ser escrito de forma mais simples utilizando o comando '.format'. Veja:
+
+print('A soma entre {} e {} totaliza:{}'.format(n1, n2, soma))
