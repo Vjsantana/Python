471e8177ac9388d6909b81f84df0022794c05db8
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-10-10 16:05:17 -0300

    Update ex005.py

--- Curso em Vídeo/ex005.py
+++ Curso em Vídeo/ex005.py
@@ -3,6 +3,11 @@ a = n-1
 s = n+1
 print('O antecessor do número {} é {} e o successor é {}' .format(n, a, s))
 
+#O comando 'print' acima pode ser escrito também da seguinte forma:
+
+print('O antecessor do número {}, escrito por você, é {} e o sucessor dele é {}' .format(n, (n-1), (n+1)))
+
+
 
 
 
