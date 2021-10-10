c6fb43838319076a011159f765fb382e269c3c1e
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-10-10 16:10:17 -0300

    Update ex005

--- Curso em Vídeo/ex005.py
+++ Curso em Vídeo/ex005.py
@@ -1,7 +1,7 @@
 n = int(input("Escreva um número"))
 a = n-1
 s = n+1
-print('O antecessor do número {} é {} e o successor é {}' .format(n, a, s))
+print('O antecessor do número {} é {} e o sucessor é {}' .format(n, a, s))
 
 #O comando 'print' acima pode ser escrito também utilizando apenas uma variável. Veja:
 
