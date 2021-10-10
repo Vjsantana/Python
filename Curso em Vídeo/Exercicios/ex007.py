1e4e8b81951562b0f73150ceb9621ce8297269f6
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-10-10 16:41:25 -0300

    Create ex007.py

---
+++ Curso em Vídeo/Exercicios/ex007.py
@@ -0,0 +1,5 @@
+n1 = int(input('Escreva o primeiro número:'))
+n2 = int(input('Escreva o segundo número:'))
+m = (n1 + n2)/2
+print('A média aritmética dos números {} e {} é {}' .format(n1, n2, m))
+
