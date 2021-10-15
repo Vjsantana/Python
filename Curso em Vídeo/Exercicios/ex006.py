d801c45ac69af803223fb37e92a9f5ffb17ecc9e
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-10-15 19:34:12 -0300

    Update ex006.py

--- Curso em Vídeo/Exercicios/ex006.py
+++ Curso em Vídeo/Exercicios/ex006.py
@@ -2,7 +2,8 @@ n = int(input('Digite um número'))
 print('O dobro do número {} é {}, o triplo é {} e a raíz quadarada é {:.2f}' .format(n, (2*n), (3*n), (n**(1/2))))
 
 """A expressão '{:.2f}' dentro da terceira máscara, que representa a potenciação ou raiz quadrada, indica 
-queremos o resultado com no máximo até duas casas decimais. Isso pode ser utilizado para quantas """
+queremos o resultado com no máximo até duas casas decimais. Isso pode ser utilizado para quantas casas 
+decimais forem necessárias."""
 
 #A potenciação ou raíz quadrada também poderia ser resolvida utilizando a função "pow(n, 1/2)".
 
